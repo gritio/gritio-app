e9978bff2a1af5936a969eb6af5ff82f8d9758c2
Sowmya Iyer <sowmyaiyer@Sowmyas-MacBook-Pro.local>
2026-01-07 11:40:37 +0000

    Adding docker

--- src/api/prisma.config.ts
+++ src/api/prisma.config.ts
@@ -1,13 +1,6 @@
-import 'dotenv/config';
-import * as path from 'path';
-
-const envPath = path.resolve(__dirname, '../../.env');
-require('dotenv').config({ path: envPath });
-import { defineConfig, env } from 'prisma/config';
-
-export default defineConfig({
+module.exports = {
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL,
   },
-});
+};
